2659e0e057d3ca1ddb7fead1062c803870be0772
xieyezi <1435398529@qq.com>
2022-12-02 10:58:30 +0800

    fix: ts warning

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,5 +1,5 @@
+//@ts-nocheck
 import DefaultTheme from "vitepress/theme"
-// @ts-ignore:next-line
 import Layout from "../../src/components/Layout.vue"
 import Year from "../../src/components/Year.vue"
 import "../../main.css"
